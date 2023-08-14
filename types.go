c66451b53d017a2cda8fc04fa78cf353f38c535c
Alexander Zaytsev <me@axv.email>
2023-08-14 12:50:35 +0300

    types correction

--- types.go
+++ types.go
@@ -26,12 +26,12 @@ const (
 
 // MarshalJSON implements the json.Marshaler interface.
 func (r *Role) MarshalJSON() ([]byte, error) {
-	return marshalJSON[Role](r, RoleSystem, RoleUser, RoleAssistant)
+	return marshalJSON(r, RoleSystem, RoleUser, RoleAssistant)
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
 func (r *Role) UnmarshalJSON(b []byte) error {
-	return unMarshalJSON[Role](r, b, RoleSystem, RoleUser, RoleAssistant)
+	return unMarshalJSON(r, b, RoleSystem, RoleUser, RoleAssistant)
 }
 
 // Model is a type of AI model name.
@@ -47,12 +47,12 @@ const (
 
 // MarshalJSON implements the json.Marshaler interface.
 func (m *Model) MarshalJSON() ([]byte, error) {
-	return marshalJSON[Model](m, ModelGPT35Turbo, ModelGPT35TurboK16, ModelGPT4, ModelGPT4K32)
+	return marshalJSON(m, ModelGPT35Turbo, ModelGPT35TurboK16, ModelGPT4, ModelGPT4K32)
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
 func (m *Model) UnmarshalJSON(b []byte) error {
-	return unMarshalJSON[Model](m, b, ModelGPT35Turbo, ModelGPT35TurboK16, ModelGPT4, ModelGPT4K32)
+	return unMarshalJSON(m, b, ModelGPT35Turbo, ModelGPT35TurboK16, ModelGPT4, ModelGPT4K32)
 }
 
 // FinishReason is a type of response finish reason.
@@ -66,21 +66,29 @@ const (
 
 // MarshalJSON implements the json.Marshaler interface.
 func (f *FinishReason) MarshalJSON() ([]byte, error) {
-	return marshalJSON[FinishReason](f, FinishReasonLength, FinishReasonStop)
+	return marshalJSON(f, FinishReasonLength, FinishReasonStop)
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
 func (f *FinishReason) UnmarshalJSON(b []byte) error {
-	return unMarshalJSON[FinishReason](f, b, FinishReasonLength, FinishReasonStop)
+	return unMarshalJSON(f, b, FinishReasonLength, FinishReasonStop)
+}
+
+// StringCommonType is a generic interface for custom string based types.
+type StringCommonType interface {
+	Role | Model | FinishReason
 }
 
 // marshalJSON is a generic function for custom types JSON marshal.
-func marshalJSON[T Role | Model | FinishReason](t *T, values ...T) ([]byte, error) {
-	v := *t
+func marshalJSON[T StringCommonType](t *T, values ...T) ([]byte, error) {
+	var (
+		v = *t
+		s = string(v)
+	)
 
 	for _, value := range values {
 		if v == value {
-			return json.Marshal(string(v))
+			return json.Marshal(s)
 		}
 	}
 
@@ -88,13 +96,14 @@ func marshalJSON[T Role | Model | FinishReason](t *T, values ...T) ([]byte, erro
 }
 
 // unMarshalJSON is a generic function for custom types JSON unmarshal.
-func unMarshalJSON[T Role | Model | FinishReason](t *T, b []byte, values ...T) error {
-	var str string
-	if err := json.Unmarshal(b, &str); err != nil {
-		return errors.Join(ErrUnmarshalJSON, fmt.Errorf("invalid string value: %v", str))
+func unMarshalJSON[T StringCommonType](t *T, b []byte, values ...T) error {
+	var s string
+
+	if err := json.Unmarshal(b, &s); err != nil {
+		return errors.Join(ErrUnmarshalJSON, fmt.Errorf("invalid string value: %v", string(b)))
 	}
 
-	v := T(str)
+	v := T(s)
 
 	for _, value := range values {
 		if v == value {
